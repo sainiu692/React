9969c0a6b54221c43a39b40363945e4ca6abc5f7
sainiu692 <sainiu692@gmail.com>
2025-03-07 22:34:20 +0530

    react

---
+++ namasteReact/app.js
@@ -0,0 +1,30 @@
+// const heading = React.createElement(
+//   "h1",
+//   {
+//     id: "heading",
+//   },
+//   "Hello World From React!" 
+// );
+
+//ReactElement(object)=>HTML(browser understands)
+
+// console.log(heading) //object
+
+const parent=React.createElement("div",{id:"parent"},
+    [React.createElement("div",{id:"child"},
+        [React.createElement("h1",{},"Im in h1 tag"),
+        React.createElement("h2",{},"Im in h2 tag")
+        ]
+    ),
+    React.createElement("div",{id:"child2"}, 
+        [React.createElement("h1",{},"Im in h1 tag"),
+        React.createElement("h2",{},"Im in h2 tag")
+        ]
+    )]
+  )
+  console.log(parent)
+  
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  
+  root.render(parent);
+  
